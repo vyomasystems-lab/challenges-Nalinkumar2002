9b1f04f3b4a300adf12e13c49c9ac23055efb140
Nalinkumar S <nalintes36@gmail.com>
2022-07-25 16:11:32 +0000

    update

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -2,9 +2,25 @@
 
 import cocotb
 from cocotb.triggers import Timer
+import random
 
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
 
-    cocotb.log.info('##### CTB: Develop your test here ########')
+    #cocotb.log.info('##### CTB: Develop your test here ########')
+
+    input_values=[0,1,2,3]
+    inp=[]
+    for i in range(31):
+        inp.append(random.choice(input_values))
+        exec(f'dut.inp{i}.value = {inp[i]}')
+    
+    for i in range(31):
+        dut.sel.value = i
+        await Timer(1, units='ns')
+        dut._log.info(f"Given input => {eval(f'dut.inp{i}.value')}\n Select line {i}  \n Expected Output => {inp[i]}  \n Output => {int(eval(f'dut.out.value'))} ")
+        assert int(dut.out.value) == inp[i], "Incorrect Output"
+
+    
+
