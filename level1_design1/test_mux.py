601d269b300d9eeca3549e14349da7399790d901
Nalinkumar S <nalintes36@gmail.com>
2022-07-25 16:29:55 +0000

    update mux

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -19,8 +19,16 @@ async def test_mux(dut):
     for i in range(31):
         dut.sel.value = i
         await Timer(1, units='ns')
-        dut._log.info(f"Given input => {eval(f'dut.inp{i}.value')}\n Select line {i}  \n Expected Output => {inp[i]}  \n Output => {int(eval(f'dut.out.value'))} ")
+        dut._log.info(f"Given input => {eval(f'dut.inp{i}.value')}\n Select line => {i}  \n Expected Output => {inp[i]}  \n Output => {int(eval(f'dut.out.value'))} ")
         assert int(dut.out.value) == inp[i], "Incorrect Output"
 
-    
+@cocotb.test()
+async def test_mux_1(dut):
+    dut.inp30.value = 1
+    dut.sel.value = 30
+    await Timer(1, units='ns')
+    dut._log.info(f"Given input => {dut.inp30.value}\n Select line : {30}  \n Expected Output => {1}  \n Output => {int(dut.out.value)} ")    
+    assert int(dut.out.value) == int(dut.inp30.value), "Incorrect Output"
+
+
 
